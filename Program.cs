032f0cacaa6d9a4793c1f17b5b176940f4b4afb2
Tuan <nguyenhahongtuan@gmail.com>
2024-07-10 21:03:09 +0700

    feat: add function is done

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ namespace Week_1
 
         static double Cong(double num1, double num2)
         {
-            return 0;
+            return num1 + num2;
         }
 
         static double Tru(double num1, double num2)
