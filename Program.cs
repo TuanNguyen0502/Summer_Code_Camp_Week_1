33c5ddcb473195057553e7452cee3752ed02dde5
Tuan <nguyenhahongtuan@gmail.com>
2024-07-10 16:35:27 +0700

    Example code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+
+        public double Cong(double num1, double num2)
+        {
+            
+        }
+
+        public double Tru(double num1, double num2)
+        {
+
+        }
+
+        public double Nhan(double num1, double num2)
+        {
+
+        }
+
+        public double Chia(double num1, double num2)
+        {
+
+        }
+
+        public double GiaiThua(double num1, double num2)
+        {
+
+        }
+    }
+}
